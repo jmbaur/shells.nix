19303048b7b85bba4392d3afd029e6a202780264
Jared Baur <jaredbaur@fastmail.com>
2023-09-20 23:48:03 -0700

    Add e2fsprogs to u-boot shell

--- u-boot.nix
+++ u-boot.nix
@@ -1,4 +1,4 @@
-{ mkShell, gnumake, bison, flex, swig, lz4, armTrustedFirmwareTools, python3, openssl, libuuid, gnutls }:
+{ mkShell, gnumake, bison, flex, swig, lz4, armTrustedFirmwareTools, e2fsprogs, python3, openssl, libuuid, gnutls }:
 # NOTE: enough to run `make check` in u-boot's source tree
 mkShell {
   name = "u-boot-shell";
@@ -9,6 +9,7 @@ mkShell {
     swig
     lz4
     armTrustedFirmwareTools
+    e2fsprogs
     (python3.withPackages (p: with p; [
       filelock
       libfdt
