929897df791241771acf39cd441a276ef55aecdb
Jared Baur <jaredbaur@fastmail.com>
2023-09-21 18:32:47 -0700

    Add many more packages to u-boot dev shell

--- u-boot.nix
+++ u-boot.nix
@@ -1,4 +1,22 @@
-{ mkShell, gnumake, bison, flex, swig, lz4, armTrustedFirmwareTools, e2fsprogs, python3, openssl, libuuid, gnutls }:
+{ mkShell
+, gnumake
+, bison
+, flex
+, swig
+, lz4
+, armTrustedFirmwareTools
+, e2fsprogs
+, SDL2
+, cbfstool
+, efitools
+, sbsigntool
+, gptfdisk
+, guestfs-tools
+, python3
+, openssl
+, libuuid
+, gnutls
+}:
 # NOTE: enough to run `make check` in u-boot's source tree
 mkShell {
   name = "u-boot-shell";
@@ -10,6 +28,13 @@ mkShell {
     lz4
     armTrustedFirmwareTools
     e2fsprogs
+    SDL2
+    cbfstool
+    efitools
+    sbsigntool
+    gptfdisk
+    guestfs-tools
+    # TODO(jared): use test/py/requirements.txt
     (python3.withPackages (p: with p; [
       coverage
       filelock
